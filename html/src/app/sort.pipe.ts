c3833ae358f04f8c652546308cdabec275c06be7
RobertPringle <Pringle@Roberts-Air.34ChurchStreet.sophists.com>
2017-03-14 19:51:27 -0400

    fixes device sort pipe

--- html/src/app/sort.pipe.ts
+++ html/src/app/sort.pipe.ts
@@ -10,6 +10,9 @@ export class SortPipe implements PipeTransform {
 
 	transform( devices: Device[] ) {
 
+		// For when if Devices has not yet been populated
+		if (devices == null) return null;
+		
 		return devices.sort( function(a:Device,b:Device) {
 			var nameA:string = a.name.toUpperCase();
 			var nameB:string = b.name.toUpperCase();
@@ -23,20 +26,6 @@ export class SortPipe implements PipeTransform {
 
 			return 0;
 		});
-		
-		/*return devices.sort(function(a, b) {
-		  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-		  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-		  if (nameA < nameB) {
-		    return -1;
-		  }
-		  if (nameA > nameB) {
-		    return 1;
-		  }
-
-		  // names must be equal
-		  return 0;
-		}); */
 
 	}
 
