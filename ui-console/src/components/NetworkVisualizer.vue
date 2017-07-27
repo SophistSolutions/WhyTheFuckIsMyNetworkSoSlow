f28dafe77c79197d987a89f50f66ed3067066507
Robert Pringle <robert.lemos.pringle@gmail.com>
2017-07-27 18:43:26 -0400

    Removes starting data from vue project and starts to migrate angular project into vue

---
+++ ui-console/src/components/NetworkVisualizer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="network-visualizer">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'network-visualizer',
+  data () {
+    return {
+      title: 'Network Visualizer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1 {
+    color: #369;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 250%;
+  }
+
+  body {
+    padding: 0px 0px 0px 0px;
+    margin: 0px 0px 0px 0px;
+  }
+
+  #deviceInformation {
+    min-height: 120px;
+    height: 20vh;
+  }
+
+  .well {
+    margin-bottom:0px;
+  }
+
+  .row {
+    margin-bottom: 0px;
+  }
+
+  .rounded {
+    border-radius: 15px;
+  }
+
+  .unround {
+    border-radius: 0px;
+  }
+
+
+</style>
