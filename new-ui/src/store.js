e554c75feae0cc61cd7f45d7f239763d71865a0b
RobertLemosPringle <robert.lemos.pringle@gmail.com>
2018-12-25 00:51:45 -0500

    Clean up code

--- new-ui/src/store.js
+++ new-ui/src/store.js
@@ -6,13 +6,7 @@ export const store = {
     this.state.values.push(newNumber);
   },
   removeValue: function(newNumber) {
-
     var index = this.state.values.indexOf(newNumber);
-
-    console.log(index)
-if (index > -1) {
-  this.state.values.splice(index, 1);
-}
-    //this.state.values.remove(newNumber);
+    if (index > -1) { this.state.values.splice(index, 1); }
   }
 };
