9bb4479127eb58fa43a37a91e32b941ff39a9497
RobertLemosPringle <robert.lemos.pringle@gmail.com>
2018-12-26 22:38:51 -0500

    Basic working first pass at wtfimnss ui

---
+++ wtfimnss-ui/src/proxy/api.ts
@@ -0,0 +1,10 @@
+import { INetwork } from '@/models/network';
+
+export function fetchNetworks(): Promise<INetwork[]> {
+    return fetch('http://192.168.244.187:8080/networks?recurse=true')
+    .then((response) => response.json())
+    .then((data) => {
+        return data;
+    })
+    .catch((error) => console.error(error));
+}
---
+++ wtfimnss-ui/src/views/Network.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="network">
+  <div v-for="network in networks" :key="network.id">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Network ID : {{ network.id }}</h1>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { INetwork } from '@/models/network.ts';
+
+import { fetchNetworks } from '@/proxy/api.ts';
+//import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+let test : INetwork = {id : "192.168.244.0/24"}
+
+
+
+@Component({
+    name : "Network"
+})
+export default class Network extends Vue {
+    networks: INetwork[] = [];
+
+    async created() {
+      debugger
+      // TODO break out network from promise
+      
+      this.networks = await fetchNetworks();
+
+    }
+
+    // created(){
+
+    //   fetchNetworks()
+    //     .then((networks: INetwork[]) => {
+    //       this.networks = networks;
+    //     })
+    // }
+}
+</script>
