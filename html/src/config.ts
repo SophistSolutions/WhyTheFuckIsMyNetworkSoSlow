841871eca5ea7e15f2da586f2b61300437133926
RobertLemosPringle <robert.lemos.pringle@gmail.com>
2019-01-22 21:20:42 -0500

    Fix API_ROOT config var by making it use window.location if VUE_APP_ROOT_API not explicitly set

--- html/src/config.ts
+++ html/src/config.ts
@@ -1,7 +1,8 @@
 // https://cli.vuejs.org/guide/mode-and-env.html
 // https://alligator.io/vuejs/working-with-environment-variables/
 
-export const API_ROOT = process.env.VUE_APP_ROOT_API;
+export const API_ROOT = process.env.VUE_APP_ROOT_API ? process.env.VUE_APP_ROOT_API : 
+    window.location.protocol + '//' + window.location.hostname + ':' + process.env.VUE_APP_DEFAULT_API_PORT;
 
 export const API_NETWORKS_PATH = process.env.VUE_APP_API_NETWORKS_PATH;
 
