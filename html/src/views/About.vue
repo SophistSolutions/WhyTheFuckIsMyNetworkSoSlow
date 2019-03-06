a45182a8c26804f264323599dde2f170fb32e2f7
Lewis Pringle <lewis@sophists.com>
2019-03-06 17:18:57 -0500

    tweaks to about box

--- html/src/views/About.vue
+++ html/src/views/About.vue
@@ -2,6 +2,18 @@
   <div class="about">
     <h1>About Why The Fuck is My Network So Slow</h1>
     <div>my version# {{ about.applicationVersion }}</div>
+
+    <div>
+      Written by
+      <ul>
+        <li>Bob Pringle <a href="https://github.com/robertpringle">github</a></li>
+        <li>Lewis G. Pringle, Jr. <a href="https://www.linkedin.com/in/lewispringle/">LinkedIn</a> <a href="https://github.com/LewisPringle">github</a> /li>
+      </ul>
+    </div>
+
+  <div>
+  @todo add link to where to post feedback (JIRA? or githib issues)
+  </div>
   </div>
 </template>
 
