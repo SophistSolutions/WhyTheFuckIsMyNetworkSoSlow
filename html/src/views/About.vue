c2310e1350817ca8be50096e921661acd3301065
Lewis Pringle <lewis@sophists.com>
2019-03-06 17:21:29 -0500

    cosmetic

--- html/src/views/About.vue
+++ html/src/views/About.vue
@@ -3,42 +3,41 @@
     <h1>About Why The Fuck is My Network So Slow</h1>
     <div>my version# {{ about.applicationVersion }}</div>
 
-    <div>
-      Written by
+    <div>Written by
       <ul>
-        <li>Bob Pringle <a href="https://github.com/robertpringle">github</a></li>
-        <li>Lewis G. Pringle, Jr. <a href="https://www.linkedin.com/in/lewispringle/">LinkedIn</a> <a href="https://github.com/LewisPringle">github</a> /li>
+        <li>Bob Pringle
+          <a href="https://github.com/robertpringle">github</a>
+        </li>
+        <li>Lewis G. Pringle, Jr.
+          <a href="https://www.linkedin.com/in/lewispringle/">LinkedIn</a> |
+          <a href="https://github.com/LewisPringle">github</a>
+        </li>
       </ul>
     </div>
 
-  <div>
-  @todo add link to where to post feedback (JIRA? or githib issues)
-  </div>
+    <div>@todo add link to where to post feedback (JIRA? or githib issues)</div>
   </div>
 </template>
 
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import { IAbout } from '@/models/IAbout'
+import Vue from "vue";
+import Component from "vue-class-component";
+import { IAbout } from "@/models/IAbout";
 
 @Component({
-  props: {
-  },
+  props: {}
 })
 export default class About extends Vue {
-
   constructor() {
     super();
   }
-    private created() {
-      this.$store.dispatch('fetchAboutInfo')
-    }
+  private created() {
+    this.$store.dispatch("fetchAboutInfo");
+  }
 
-    private get about(): IAbout {
-      return this.$store.getters.getAboutInfo;
-    }
-
+  private get about(): IAbout {
+    return this.$store.getters.getAboutInfo;
+  }
 }
 </script>
