b59353bda94e2f78fb7bdc3009336bcffff71141
Lewis Pringle <lewis@sophists.com>
2019-03-09 15:48:55 -0500

    use join() to comma separate types and internet addresses (dont show array)

--- html/src/components/Device.vue
+++ html/src/components/Device.vue
@@ -6,10 +6,10 @@
       </v-card-title>
 
       <v-card-text>
-        <p v-if="device.type">Type: {{ device.type }}</p>
+        <p v-if="device.type">Type: {{ device.type.join(", ") }}</p>
         <p v-if="device.operatingSystem">OS: {{ device.operatingSystem }}</p>
         <p v-if="device.presentationURL"><a href="{{ device.presentationURL }}" target="_blank">Open Device</a></p>
-        <p>Internet Addresses: {{ device.internetAddresses }}</p>
+        <p>Internet Addresses: {{ device.internetAddresses.join(", ") }}</p>
       </v-card-text>
     </v-card>
   </div>
