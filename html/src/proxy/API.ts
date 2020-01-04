9e0f24fc15e4f7ac4a7aa5a08913ef7daed3d7e3
Lewis Pringle <lewis@sophists.com>
2020-01-04 18:07:25 -0500

    fixed typo

--- html/src/proxy/API.ts
+++ html/src/proxy/API.ts
@@ -39,7 +39,7 @@ export async function fetchDevices(searchCriteria?: ISortBy): Promise<IDevice[]>
     return fetch(API_ROOT + `/devices?recurse=true&sort=${encodeURI(JSON.stringify (searchSpecs))}`)
     .then((response) => response.json())
     .then((data) => {
-        data.forEach( (d) => {
+        data.forEach( (d: any) => {
             // fixup urls that are relative to be relative to the WSAPI
             if (d.icon) {
                 d.icon = new URL (d.icon, API_ROOT);
