123bd02af340ffcf27056f7cf05885527d5446c1
Lewis Pringle <lewis@sophists.com>
2020-02-13 11:40:57 -0500

    Added .prettierrc (and ran in a few files) to address issue with defaults messing up arrow functions causing linting warnings

--- html/src/proxy/API.ts
+++ html/src/proxy/API.ts
@@ -1,63 +1,59 @@
 import { IDevice } from "@/models/device/IDevice";
-import { ISortBy, SearchSpecification, SortFieldEnum  } from "@/models/device/SearchSpecification";
+import { ISortBy, SearchSpecification, SortFieldEnum } from "@/models/device/SearchSpecification";
 import { IAbout } from "@/models/IAbout";
 import { INetwork } from "@/models/network/INetwork";
 
 import { API_ROOT } from "@/config";
 
-
 export async function fetchNetworks(): Promise<INetwork[]> {
-    return fetch(API_ROOT + `/networks?recurse=true`)
+  return fetch(API_ROOT + `/networks?recurse=true`)
     .then((response) => response.json())
     .then((data) => {
-        return data;
+      return data;
     })
     .catch((error) => console.error(error));
 }
 
 export async function fetchDevices(searchCriteria?: ISortBy): Promise<IDevice[]> {
-    // @todo make these search params depend on parameters,
-    // and especially make compareNetwork depend on current active network
-    // (and maybe sometimes omit)
+  // @todo make these search params depend on parameters,
+  // and especially make compareNetwork depend on current active network
+  // (and maybe sometimes omit)
 
-    const searchSpecification: ISortBy[] = [];
+  const searchSpecification: ISortBy[] = [];
 
-    if (!searchCriteria) {
-        searchSpecification.push({by: SortFieldEnum.ADDRESS, ascending: true});
-        searchSpecification.push({by: SortFieldEnum.TYPE, ascending: true});
-        searchSpecification.push({by: SortFieldEnum.PRIORITY, ascending: true});
-    } else {
-        searchSpecification.push(searchCriteria);
-    }
+  if (!searchCriteria) {
+    searchSpecification.push({ by: SortFieldEnum.ADDRESS, ascending: true });
+    searchSpecification.push({ by: SortFieldEnum.TYPE, ascending: true });
+    searchSpecification.push({ by: SortFieldEnum.PRIORITY, ascending: true });
+  } else {
+    searchSpecification.push(searchCriteria);
+  }
 
-    // TODO correct hardcoded compareNetwork
-    const searchSpecs = new SearchSpecification(
-        searchSpecification,
-        "192.168.244.0/24",
-    );
+  // TODO correct hardcoded compareNetwork
+  const searchSpecs = new SearchSpecification(searchSpecification, "192.168.244.0/24");
 
-    return fetch(API_ROOT + `/devices?recurse=true&sort=${encodeURI(JSON.stringify (searchSpecs))}`)
+  return fetch(API_ROOT + `/devices?recurse=true&sort=${encodeURI(JSON.stringify(searchSpecs))}`)
     .then((response) => response.json())
     .then((data) => {
-        data.forEach( (d: any) => {
-            // fixup urls that are relative to be relative to the WSAPI
-            if (d.icon) {
-                d.icon = new URL (d.icon, API_ROOT);
-            }
-        });
-        return data;
+      data.forEach((d: any) => {
+        // fixup urls that are relative to be relative to the WSAPI
+        if (d.icon) {
+          d.icon = new URL(d.icon, API_ROOT);
+        }
+      });
+      return data;
     })
     .then((data) => {
-        return data;
+      return data;
     })
     .catch((error) => console.error(error));
 }
 
 export async function fetchAboutInfo(): Promise<IAbout> {
-    return fetch(API_ROOT + "/about")
+  return fetch(API_ROOT + "/about")
     .then((response) => response.json())
     .then((data) => {
-        return data;
+      return data;
     })
     .catch((error) => console.error(error));
 }
