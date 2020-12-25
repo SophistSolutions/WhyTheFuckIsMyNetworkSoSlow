2dbdb0b8c664f14b50f44d065a6f42b72d397ab6
Lewis Pringle <lewis@sophists.com>
2020-12-25 15:33:12 -0500

    html: start progress proptype home page

--- html/src/views/Home.vue
+++ html/src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Why The Fuck is My Network So Slow?
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="mb-4">
+        Networks (active + favorites)
+        <ul>
+          <li>Net 1</li>
+          <div>: 23 devices, operating normally</div>
+          <li>Net 2</li>
+          <div>: 23 devices, operating normally</div>
+        </ul>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
