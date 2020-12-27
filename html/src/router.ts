a8b3365a5d13b711534d5537ad37471e0da36f6d
Lewis Pringle <lewis@sophists.com>
2020-12-26 22:33:22 -0500

    renamed views/DeviceView.vue -> views/Devices.vue

--- html/src/router.ts
+++ html/src/router.ts
@@ -76,8 +76,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "DeviceView-DEPRECATED" */ "./views/DeviceView.vue"),
+      component: () => import(/* webpackChunkName: "Devices" */ "./views/Devices.vue"),
       meta: {
         breadcrumbs: [
           { text: "Home", href: "/" },
