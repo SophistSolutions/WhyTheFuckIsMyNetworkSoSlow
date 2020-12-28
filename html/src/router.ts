fef5e0b19c157ef94a989b39d7c8ba8a047cfe77
Lewis Pringle <lewis@sophists.com>
2020-12-27 23:20:32 -0500

    html renamed:    src/views/NetworksView.vue -> src/views/Networks.vue

--- html/src/router.ts
+++ html/src/router.ts
@@ -74,7 +74,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "NetworkView" */ "./views/NetworksView.vue"),
+      component: () => import(/* webpackChunkName: "NetworkView" */ "./views/Networks.vue"),
       meta: {
         breadcrumbs: [
           { text: "Home", href: "/#/" },
