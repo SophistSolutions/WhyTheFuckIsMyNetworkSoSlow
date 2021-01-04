0b35f1138345c2d52639846fad41ee4787485655
Lewis Pringle <lewis@sophists.com>
2021-01-04 10:56:32 -0500

    update capitalizaiton in app name in home page

--- html/src/views/Home.vue
+++ html/src/views/Home.vue
@@ -4,7 +4,7 @@
     <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Why the Fuck is My Network So Slow?
+          Why The Fuck Is My Network So Slow?
         </h1>
       </v-col>
     </v-row>
