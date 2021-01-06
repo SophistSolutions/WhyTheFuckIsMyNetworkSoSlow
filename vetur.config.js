016b6585eb47c32ed188d63e3ae2d2b4a8eabfa9
Lewis Pringle <lewis@sophists.com>
2021-01-06 17:30:12 -0500

    vetur.config.js file so it finds the package.config file and editring in vscode works better

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    settings: {
+    },
+    projects: [
+      './html', // shorthand for only root.
+      {
+        root: './html',
+      }
+    ]
+  }
