a006bd073ee6c7cb5292f42fd7ed85e0efe32590
Lewis Pringle <lewis@sophists.com>
2021-12-23 16:01:08 -0500

    cosmetic

--- Backend/Sources/WebServices/Model.h
+++ Backend/Sources/WebServices/Model.h
@@ -244,7 +244,7 @@ namespace WhyTheFuckIsMyNetworkSoSlow::BackendApp::WebServices::Model {
      */
     struct Device {
 
-        Device () = default;
+        Device ()              = default;
         Device (const Device&) = default;
         Device (Device&&)      = default;
         Device& operator= (const Device&) = default;
