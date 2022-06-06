1f6bf633c4d1de0a1154925024e9e2507e4a3b3b
Lewis Pringle <lewis@sophists.com>
2022-06-06 09:24:05 -0400

    more mostly cosmetic cleanups to quasar-html app

--- html-quasar/src/pages/DevicePage.vue
+++ html-quasar/src/pages/DevicePage.vue
@@ -1,7 +1,7 @@
 
 <script setup lang="ts">
 import { defineComponent, defineProps, onMounted, onUnmounted, nextTick, ref, computed, ComputedRef } from 'vue';
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import * as moment from 'moment';
 
 import { IDevice } from "../models/device/IDevice";
@@ -21,26 +21,18 @@ import {
 import { useQuasar } from 'quasar';
 
 // Components
-// import Search from '../components/Search.vue';
-//import ClearButton from '../components/ClearButton.vue';
 import DeviceDetails from '../components/DeviceDetails.vue';
-import ReadOnlyTextWithHover from '../components/ReadOnlyTextWithHover.vue';
-import Link2DetailsPage from '../components/Link2DetailsPage.vue';
-import FilterSummaryMessage from '../components/FilterSummaryMessage.vue';
 
 import { useWTFStore } from '../stores/WTF-store'
-const $q = useQuasar()
 
+const $q = useQuasar()
 const store = useWTFStore()
 
 const props = defineProps({
   selectedNetworkink: { type: String, required: false, default: null },
 })
 
-
 let polling: undefined | NodeJS.Timeout;
-
-let allAvailableNetworks: ComputedRef<INetwork[]> = computed(() => store.getAvailableNetworks);
 
 defineComponent({
   components: {
@@ -66,13 +58,11 @@ onUnmounted(() => {
 })
 
 const route = useRoute()
-const router = useRouter()
-
 
 let device: ComputedRef<IDevice> = computed(() => {
-    let r = store.getDevice(route.params.id);
-    if (r == null) {
-      r = { id: "INVALID", attachedNetworks: {}, name: "INVALID", type: null as any };
+    let r : IDevice = store.getDevice(route.params.id as string);
+    if (!r) {
+      r = { id: "INVALID", attachedNetworks: {}, name: "INVALID", attachedNetworkInterfaces:[], type: null };
     }
     return r;
   }
@@ -86,15 +76,13 @@ let device: ComputedRef<IDevice> = computed(() => {
 </style>
 
 <template>
-  <v-container>
-    <app-bar />
-    <v-card class="deviceListCard">
+  <q-page class="col q-pa-md q-gutter-md">
+    <q-card class="deviceListCard">
       <v-card-title>
         Device {{ device == null ? "loading..." : '"' + device.name + '"' }}
-        <v-spacer></v-spacer>
       </v-card-title>
       <DeviceDetails v-if="device" class="detailsSection" :deviceId="device.id"></DeviceDetails>
-    </v-card>
-  </v-container>
+    </q-card>
+  </q-page>
 </template>
 
