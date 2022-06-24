c5e31a7abacef473ff85359111620a8d33005e4c
Lewis Pringle <lewis@sophists.com>
2022-06-24 05:54:12 -0400

    html: fixed search component, to use ToRef to watch property value and separate read/write property for text control and watch to update

--- html/src/components/Search.vue
+++ html/src/components/Search.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, Ref } from 'vue';
+import { defineProps, defineEmits, ref, Ref, ToRef, toRef, watch } from 'vue';
 
 const props = defineProps({
   searchFor: { type: String, required: false },
@@ -7,7 +7,10 @@ const props = defineProps({
 
 const emit = defineEmits(['update:searchFor'])
 
-var search: Ref<string | undefined> = ref(props.searchFor);
+var searchRO: ToRef<string | undefined> = toRef(props, 'searchFor');  // read-only reference to the prop, so we are notified if it changes
+let search: Ref<string> = ref (props.searchFor || "");                // writable prop, we can update with v-model
+
+watch (searchRO, () => search.value = props.searchFor || "" );
 
 const updateSearchFor = (newValue: string | number | null) => {
   emit('update:searchFor', newValue)
