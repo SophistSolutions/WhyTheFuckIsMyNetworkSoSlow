2fe476a2b887620434e9f6c6760f0fa1789b7a31
Lewis Pringle <lewis@sophists.com>
2022-06-24 05:23:12 -0400

    html small cleanup to ClearButton code

--- html/src/pages/NetworksPage.vue
+++ html/src/pages/NetworksPage.vue
@@ -370,7 +370,7 @@ const pagination = ref({
       <Search v-model:searchFor="search" />
       <FilterSummaryMessage dense :filtered="filtered" :nItemsSelected="filteredExtendedNetworks.length"
         :nTotalItems="allNetworks?.length" itemsName="networks" />
-      <ClearButton v-if="filtered" v-on:click="clearFilter" />
+      <ClearButton v-if="filtered" @click="clearFilter" />
     </q-toolbar>
     <q-toolbar class="justify-between secondary-toolbar">
       <q-select v-model="pageUserOptions.VisibleColumns" multiple dense options-dense :display-value="$q.lang.table.columns" emit-value
