ab42e214dd08dae36ec5a84809a3763f742e7101
Lewis Pringle <lewis@sophists.com>
2022-06-25 13:05:02 -0400

    cosmetic

--- html/src/config/config.ts
+++ html/src/config/config.ts
@@ -8,7 +8,7 @@ export const DEBUG_MODE: boolean = process.env.VUE_APP_DEBUG_MODE=='true';
 export const VUE_MY_HTML_APP_VERSION: string = process.env.VUE_MY_HTML_APP_VERSION ?? "";
 export const VUE_VERSION: string = process.env.VUE_VERSION ?? "";
 
-console.log(`API_ROOT=` + API_ROOT)
-console.log(`DEBUG_MODE=` + DEBUG_MODE)
-console.log(`VUE_MY_HTML_APP_VERSION=` + VUE_MY_HTML_APP_VERSION)
-console.log(`VUE_VERSION=` + VUE_VERSION)
+// console.log(`API_ROOT=` + API_ROOT)
+// console.log(`DEBUG_MODE=` + DEBUG_MODE)
+// console.log(`VUE_MY_HTML_APP_VERSION=` + VUE_MY_HTML_APP_VERSION)
+// console.log(`VUE_VERSION=` + VUE_VERSION)
