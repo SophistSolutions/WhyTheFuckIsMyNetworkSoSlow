fd33809491dd01c23ebf5d275d3b282fc5e3e1a4
Lewis Pringle <lewis@sophists.com>
2022-06-29 09:50:20 -0400

    fixed bug parsing process.env params as default configuraiton

--- html/src/config/config.ts
+++ html/src/config/config.ts
@@ -1,7 +1,11 @@
 // https://quasar.dev/quasar-cli-webpack/handling-process-env
 
-export const API_ROOT: string = process.env.VUE_APP_ROOT_API ? process.env.VUE_APP_ROOT_API :
-    window.location.protocol + "//" + window.location.hostname + ":" + process.env.VUE_APP_DEFAULT_API_PORT;
+
+// @todo consider optionally loading API_ROOT from localStorage as override - just for debugging
+export const API_ROOT: string = 
+    process.env.VUE_APP_ROOT_API ??
+    (window.location.protocol + "//" + window.location.hostname + ":" + (process.env.VUE_APP_DEFAULT_API_PORT ?? "8080"))
+    ;
 
 export const DEBUG_MODE: boolean = process.env.VUE_APP_DEBUG_MODE=='true';
 
