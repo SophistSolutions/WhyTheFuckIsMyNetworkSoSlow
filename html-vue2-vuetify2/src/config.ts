a79a300ba947d04261ce0259b2384072ff5d2494
Lewis Pringle <lewis@sophists.com>
2022-06-30 19:28:54 -0400

    Lose html-react, html-vue2-vuetify2, html-vue3-vuetify3: if we need to revert to any of these technologiues, can find it in git history

--- html-vue2-vuetify2/src/config.ts
+++
@@ -1,7 +0,0 @@
-// https://cli.vuejs.org/guide/mode-and-env.html
-// https://alligator.io/vuejs/working-with-environment-variables/
-
-export const API_ROOT: string = process.env.VUE_APP_ROOT_API ? process.env.VUE_APP_ROOT_API :
-    window.location.protocol + "//" + window.location.hostname + ":" + process.env.VUE_APP_DEFAULT_API_PORT;
-
-export const DEBUG_MODE: boolean = process.env.VUE_APP_DEBUG_MODE;
--- html-vue3-vuetify3/src/models/network/IInternetServiceProvider.ts
+++
@@ -1,3 +0,0 @@
-export interface IInternetServiceProvider {
-    name: string;
-}
