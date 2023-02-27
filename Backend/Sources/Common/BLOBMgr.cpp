4bbadcc86507ad7370edad36b1c8c4fa309777de
Lewis Pringle <lewis@sophists.com>
2023-02-26 20:13:25 -0500

    adjust lambda capture to avoid warning

--- Backend/Sources/Common/BLOBMgr.cpp
+++ Backend/Sources/Common/BLOBMgr.cpp
@@ -234,7 +234,7 @@ optional<GUID> BLOBMgr::AsyncAddBLOBFromURL (const URI& url, bool recheckIfExpir
         }
     }
     if (not storeGUID.has_value ()) {
-        fThreadPool_.rwget ().rwref ()->AddTask ([=, this] () {
+        fThreadPool_.rwget ().rwref ()->AddTask ([url, recheckIfExpired, this] () {
             AddBLOBFromURL (url, recheckIfExpired); // @todo if this fails (CATCH) - then negative cache, so we dont try too often
         });
     }
