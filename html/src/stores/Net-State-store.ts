86c21f2475739bd73235e3d555d6569fb0b322d3
Lewis Pringle <lewis@sophists.com>
2023-03-02 20:26:55 -0500

    lose selectedNetwork in pina store; and fixed up device loading details ids/ and basically the suppression of extra calls code - hopwfully right now

--- html/src/stores/Net-State-store.ts
+++ html/src/stores/Net-State-store.ts
@@ -16,7 +16,7 @@ import {
   fetchNetworkInterface,
 } from '../proxy/API';
 
-import {kCompileTimeConfiguration} from '../config/config';
+import { kCompileTimeConfiguration } from '../config/config';
 
 
 // @todo perhaps add in 'lasttimerequested' and 'lastTimeSuccessfulResponse' and throttle/dont request
@@ -34,28 +34,28 @@ export const useNetStateStore = defineStore('Net-State-Store', {
     rolledUpAvailableNetworkIDs: new Set() as Set<string>,
     // cache of objects, some of which maybe primary networks (rollups) and some maybe details
     networkDetails: {} as { [key: string]: INetwork },
-    loadingNetworks: {
+    loadingActiveNetworks: {
       numberOfTimesLoaded: 0,
       numberOfOutstandingLoadRequests: 0,
     } as ILoading,
+    loadingNetworkDetailsForID: new Set() as Set<string>,
     networkInterfaces: {} as { [key: string]: INetworkInterface },
-    selectedNetworkId: {} as string,
+    loadingNetworkInterfaceDetailsForID: new Set() as Set<string>,
     rolledUpDeviceIDs: new Set() as Set<string>,
     // cache of objects, some of which maybe primary devices (rollups) and some maybe details
     deviceDetails: {} as { [key: string]: IDevice },
-    devicesLoading: {
+    loadingActiveDevices: {
       numberOfTimesLoaded: 0,
       numberOfOutstandingLoadRequests: 0,
     } as ILoading,
-    // @todo - incomplete/draft attempt to track what we are already loading
-    networkInterfacesLoading: new Set() as Set<string>,
+    loadingDeviceDetailsForID: new Set() as Set<string>,
   }),
   getters: {
     getLoading_Networks: (state) => {
-      return state.loadingNetworks;
+      return state.loadingActiveNetworks;
     },
     getLoading_Devices: (state) => {
-      return state.devicesLoading;
+      return state.loadingActiveDevices;
     },
     getAvailableNetworks: (state) => {
       return [...state.rolledUpAvailableNetworkIDs].map((ni) => state.networkDetails[ni]);
@@ -64,7 +64,7 @@ export const useNetStateStore = defineStore('Net-State-Store', {
       return (id: string) => state.networkDetails[id];
     },
     getNetworkInterface: (state) => {
-      return (id: string): INetworkInterface|undefined => state.networkInterfaces[id];
+      return (id: string): INetworkInterface | undefined => state.networkInterfaces[id];
     },
     getNetworkInterfaces: (state) => {
       // untested as of 2022-11-24 - not sure the reduce part is right
@@ -80,13 +80,10 @@ export const useNetStateStore = defineStore('Net-State-Store', {
       return [...state.rolledUpDeviceIDs].map((di) => state.deviceDetails[di]);
     },
     getNonNullDevices: (state) => {
-      return [...state.rolledUpDeviceIDs].map((di) => state.deviceDetails[di]).filter ((di) => di != null);
+      return [...state.rolledUpDeviceIDs].map((di) => state.deviceDetails[di]).filter((di) => di != null);
     },
     getDevice: (state) => {
       return (id: string) => state.deviceDetails[id];
-    },
-    getSelectedNetworkId: (state) => {
-      return state.selectedNetworkId;
     },
     getAboutInfo: (state) => {
       return state.about;
@@ -94,53 +91,68 @@ export const useNetStateStore = defineStore('Net-State-Store', {
   },
   actions: {
     async fetchAvailableNetworks() {
-      const networkIDs = await fetchNetworks();
-      // for now, fetch network before adding to rolledUpAvailableNetworkIDs, but really should
-      // do other way and make calling code more OK with when details still null
-      // if you reorder with teh code as is (--LGP 2022-12-13) - get sporadic failures running hitting nulls
-      // as stuff loads
-      networkIDs.forEach(async (i:string) => await this.fetchNetwork(i));
-      networkIDs.forEach (async (i:string)  => this.rolledUpAvailableNetworkIDs.add (i))
+      if (this.loadingActiveNetworks.numberOfOutstandingLoadRequests > 2) {
+        console.log('Suppressing fetchAvailableNetworks due to WSAPI delays');
+        return;
+      }
+      this.loadingActiveNetworks.numberOfOutstandingLoadRequests++;
+      let networkIDs = [];
+      try {
+        networkIDs = await fetchNetworks();
+        // for now, fetch network before adding to rolledUpAvailableNetworkIDs, but really should
+        // do other way and make calling code more OK with when details still null
+        // if you reorder with teh code as is (--LGP 2022-12-13) - get sporadic failures running hitting nulls
+        // as stuff loads
+        this.rolledUpAvailableNetworkIDs.clear();//UNSURE but i think best
+        networkIDs.forEach(async (i: string) => this.rolledUpAvailableNetworkIDs.add(i))
+        this.loadingActiveNetworks.numberOfTimesLoaded++;
+      }
+      finally {
+        this.loadingActiveNetworks.numberOfOutstandingLoadRequests--;
+      }
+      //  %todo reconsider - no need to fetch these just cuz we fetched network ids - many need if NEW ones (maybe track those) - but otherwise let soemting
+      // exlse express interest in that id, and fetch it --LGP 2023-03-02
+      networkIDs.forEach(async (i: string) => await this.fetchNetwork(i));
     },
     async fetchNetworkInterfaces(ids: string[]) {
       ids.forEach(async (i: string) => {
-        if (this.networkInterfacesLoading.has(i)) {
+        if (this.loadingNetworkInterfaceDetailsForID.has(i)) {
           console.log(
             `Suppressing fetchNetworkInterface ${i} since in progress (throttling)`
           );
         } else {
-          this.networkInterfacesLoading.add(i);
+          this.loadingNetworkInterfaceDetailsForID.add(i);
           try {
             this.networkInterfaces[i] = await fetchNetworkInterface(i);
           } finally {
-            this.networkInterfacesLoading.delete(i);
+            this.loadingNetworkInterfaceDetailsForID.delete(i);
           }
         }
       });
     },
     async fetchNetworks(ids: string[]) {
-      // primitive WSAPI throttling
-      if (this.devicesLoading.numberOfOutstandingLoadRequests > 2) {
-        console.log('Suppressing fetchNetworks due to WSAPI delays');
-        return;
-      }
-      this.loadingNetworks.numberOfOutstandingLoadRequests++;
-      try {
-        ids.forEach( async (id) => await this.fetchNetwork(id) );
-        this.loadingNetworks.numberOfTimesLoaded++;
-      } finally {
-        this.loadingNetworks.numberOfOutstandingLoadRequests--;
-      }
+      ids.forEach(async (id) => await this.fetchNetwork(id));
     },
-    async fetchNetwork(id: string) {
-      // Suprising and disapointing that Equals optimization helps so much
-      // I would have expected Pinia to do this check
-      const r = await fetchNetwork(id);
-      if (!Equals (r, this.networkDetails[id])) {
-        if (kCompileTimeConfiguration.DEBUG_MODE) {
-          console.log("new network value: id=", r.id)
+    async fetchNetwork(i: string) {
+      if (this.loadingNetworkDetailsForID.has(i)) {
+        console.log(
+          `Suppressing fetchNetwork ${i} since in progress (throttling)`
+        );
+      } else {
+        this.loadingNetworkDetailsForID.add(i);
+        try {
+          // Suprising and disapointing that Equals optimization helps so much
+          // I would have expected Pinia to do this check
+          const r = await fetchNetwork(i);
+          if (!Equals(r, this.networkDetails[i])) {
+            if (kCompileTimeConfiguration.DEBUG_MODE) {
+              console.log("new network value: id=", r.id)
+            }
+            this.networkDetails[i] = r;
+          }
+        } finally {
+          this.loadingNetworkDetailsForID.delete(i);
         }
-        this.networkDetails[id] = r;
       }
     },
     async fetchAboutInfo() {
@@ -148,36 +160,48 @@ export const useNetStateStore = defineStore('Net-State-Store', {
     },
     async fetchActiveDevices(searchSpecs?: ISortBy) {
       // primitive WSAPI throttling
-      if (this.devicesLoading.numberOfOutstandingLoadRequests > 2) {
+      if (this.loadingActiveDevices.numberOfOutstandingLoadRequests > 2) {
         console.log('Suppressing fetchActiveDevices due to WSAPI delays');
         return;
       }
-      this.devicesLoading.numberOfOutstandingLoadRequests++;
+      this.loadingActiveDevices.numberOfOutstandingLoadRequests++;
+      let devices: string[] = [];
       try {
-        const devices: string[] = await fetchDevices(searchSpecs);
-        devices.forEach (async (i:string)  => this.rolledUpDeviceIDs.add (i))
-        this.fetchDevices(devices);
-        this.devicesLoading.numberOfTimesLoaded++;
+        devices = await fetchDevices(searchSpecs);
+        this.rolledUpDeviceIDs.clear(); // unclear but probably sensible - careful of reactivity
+        devices.forEach(async (i: string) => this.rolledUpDeviceIDs.add(i))
+        this.loadingActiveDevices.numberOfTimesLoaded++;
       } finally {
-        this.devicesLoading.numberOfOutstandingLoadRequests--;
+        this.loadingActiveDevices.numberOfOutstandingLoadRequests--;
       }
+      //  %todo reconsider - no need to fetch these just cuz we fetched devices ids - many need if NEW ones (maybe track those) - but otherwise let soemting
+      // exlse express interest in that id, and fetch it --LGP 2023-03-02
+      this.fetchDevices(devices); // after reset numberOutstanding...
     },
-    async fetchDevice(id: string) {
-      // Suprising and disapointing that Equals optimization helps so much
-      // I would have expected Pinia to do this check
-      const r = await fetchDevice(id);
-      if (!Equals (r, this.deviceDetails[id])) {
-        if (kCompileTimeConfiguration.DEBUG_MODE) {
-          console.log("new device value: named=", r.name)
+    async fetchDevice(i: string) {
+      if (this.loadingDeviceDetailsForID.has(i)) {
+        console.log(
+          `Suppressing fetchDevice ${i} since in progress (throttling)`
+        );
+      } else {
+        this.loadingDeviceDetailsForID.add(i);
+        try {
+          // Suprising and disapointing that Equals optimization helps so much
+          // I would have expected Pinia to do this check
+          const r = await fetchDevice(i);
+          if (!Equals(r, this.deviceDetails[i])) {
+            if (kCompileTimeConfiguration.DEBUG_MODE) {
+              console.log("new device value: named=", r.name)
+            }
+            this.deviceDetails[i] = r;
+          }
+        } finally {
+          this.loadingDeviceDetailsForID.delete(i);
         }
-        this.deviceDetails[id] = r;
       }
     },
     async fetchDevices(ids: Array<string>) {
       ids.forEach(async (i) => (this.fetchDevice(i)));
-    },
-    setSelectedNetwork(networkId: string) {
-      this.selectedNetworkId = networkId;
     },
   },
 });
