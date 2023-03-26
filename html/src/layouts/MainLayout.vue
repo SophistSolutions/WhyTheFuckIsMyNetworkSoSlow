e4e1f5f5f0a0d5bdf584af4bf83d55e70aca579b
Lewis Pringle <lewis@sophists.com>
2023-03-26 09:27:25 -0400

    https://github.com/SophistSolutions/WhyTheFuckIsMyNetworkSoSlow/issues/87 - added main menu to left drawer side; about to cahgne RHS to context menu

--- html/src/layouts/MainLayout.vue
+++ html/src/layouts/MainLayout.vue
@@ -12,46 +12,28 @@ const router = useRouter();
 // Leave around stuff from sample layout, for a bit...
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Home',
+    caption: 'Overview',
+    icon: 'home',
+    link: '/#/',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
+    title: 'About',
+    caption: 'About this application',
+    icon: 'api',
+    link: '/#/about',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
+    title: 'Devices',
+    caption: 'All devices (or filter)',
+    icon: 'devices',
+    link: '/#/devices',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
+    title: 'Networks',
+    caption: 'All networks (or filter)',
+    icon: 'wifi',
+    link: '/#/networks',
   },
 ];
 
@@ -127,7 +109,7 @@ onMounted(() => {
 
     <q-drawer v-model="leftDrawerOpen" bordered>
       <q-list>
-        <q-item-label header>Essential Links</q-item-label>
+        <q-item-label header>WTF Objects</q-item-label>
         <EssentialLink
           v-for="link in linksList"
           :key="link.title"
