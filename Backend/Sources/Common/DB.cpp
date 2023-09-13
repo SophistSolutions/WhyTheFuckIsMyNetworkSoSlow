fcc4e631cfe2e6afed348c3bddbf9343d757dd10
Lewis Pringle <lewis@sophists.com>
2023-09-13 14:48:27 -0400

    go back to Options::ThreadingMode::eMultiThread;

--- Backend/Sources/Common/DB.cpp
+++ Backend/Sources/Common/DB.cpp
@@ -81,8 +81,9 @@ WhyTheFuckIsMyNetworkSoSlow::BackendApp::Common::DB::DB (Version targetDBVersion
 SQL::Connection::Ptr WhyTheFuckIsMyNetworkSoSlow::BackendApp::Common::DB::NewConnection ()
 {
     // Logically I THINK (from docs) - should use eMultiThread but experiemnt with eSerialized to see if fixes sporadic failures (mostly on unix)- but maybe just now saw on windows - flying...--LGP 2023-03-92
-    //constexpr auto kThreadModel_ = Options::ThreadingMode::eMultiThread;
-    constexpr auto kThreadModel_ = Options::ThreadingMode::eSerialized;
+    // Serialized didn't hlep - still get tons of 'Device or resource busy on database extion lastsql select * from BLOBURL'...., (at least wtih stk 2.1) - so go back to multithread - makes more sense -- LGP 2023-09-13
+    constexpr auto kThreadModel_ = Options::ThreadingMode::eMultiThread;
+    //constexpr auto kThreadModel_ = Options::ThreadingMode::eSerialized;
     auto           dbPath        = pFileName ();
     filesystem::create_directories (dbPath.parent_path ());
 #if __cpp_designated_initializers
