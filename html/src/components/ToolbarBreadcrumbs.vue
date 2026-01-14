e0f1abae885f3a7a9befe88fa16d851c0cbaccb7
Lewis Pringle <lewis@sophists.com>
2026-01-14 14:38:50 -0500

    cosmetic

--- html/src/components/ToolbarBreadcrumbs.vue
+++ html/src/components/ToolbarBreadcrumbs.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref, Ref, ToRef, toRef, watch } from 'vue';
+import { ref, Ref, ToRef, toRef, watch } from "vue";
 
 const props = defineProps({
   breadcrumbs: { type: Array, required: true },
 });
 
 // Based on code I had for Search.vue - not super logical - need to cleanup/better understand vue3 reactivity... LGP 2022-11-25
-var breadcrumbsRO: ToRef<object | undefined> = toRef(props, 'breadcrumbs'); // read-only reference to the prop, so we are notified if it changes
+var breadcrumbsRO: ToRef<object | undefined> = toRef(props, "breadcrumbs"); // read-only reference to the prop, so we are notified if it changes
 let breadcrumbs: Ref<object> = ref(props.breadcrumbs || []); // writable prop, we can update with v-model
 
 watch(breadcrumbsRO, () => {
